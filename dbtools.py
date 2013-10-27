a0e957e994e583a7c15c2f53fa6de672d0357247
Angela Gong <anjoola@anjoola.com>
2013-10-26 20:10:31 -0700

    Created new dbtools file for database-specific functions.

---
+++ dbtools.py
@@ -0,0 +1,75 @@
+"""
+dbtools
+-------
+Contains helper methods involving the database and queries.
+"""
+
+import mysql.connector
+import sqlparse
+
+
+def source_files(files, cursor):
+  """
+  source_files
+  ------------
+  Sources files into the database. Since the "source" command is for the
+  MySQL command-line interface, we have to parse the source file and run
+  each command one at a time.
+
+  files: The source files to source.
+  cursor: The database cursor.
+  """
+  if len(files) <= 0:
+    return
+
+  # Loop through each source file.
+  for source in files:
+    sql = open(source).read()
+    sql_list = sqlparse.split(sql)
+    for sql in sql_list:
+      if len(sql.strip()) == 0:
+        continue
+      cursor.execute(sql)
+
+
+def get_schema(cursor):
+  """
+  schema
+  ------
+  Gets the schema of the result. Returns a list of tuples, where each tuple is
+  of the form (column_name, type, None, None, None, None, null_ok, flags).
+
+  cursor: The database cursor.
+  returns: A list of tuples representing the schema.
+  """
+  return cursor.description
+
+
+def run_query(setup, query, cursor):
+  """
+  run_query
+  ---------
+  Runs a query and does all the setup and teardown required for the query.
+
+  setup: A JSON object containing the setup for the query.
+  query: The query to run.
+  cursor: The database cursor.
+
+  returns: A tuple of the form (schema, results) where schema is the schema
+           of the results.
+  """
+  results = []
+
+  # Source files to insert data needed for the query.
+  source_files(setup["source"], cursor)
+
+  # Query setup.
+  cursor.execute(setup["setup"])
+  cursor.execute(query)
+
+  # Get the query results and schema.
+  results = [str(row) for row in cursor]
+  schema = get_schema(cursor)
+
+  cursor.execute(setup["teardown"])
+  return (schema, results)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import argparse, sqlparse
+import argparse
+import dbtools
 import json
 import mysql.connector
 import os, sys
@@ -60,21 +61,23 @@ def grade(specs, filename):
       cursor = DB.cursor()
 
       # Run the test code.
-      expected = run_query(test, test["query"], cursor)
+      expected = dbtools.run_query(test, test["query"], cursor)
       # Run the student's code.
       try:
         # If there are comments to grade for this problem, print them out.
         if needs_comments == "true":
           print responses[problem_num].comments
-        result = run_query(test, responses[problem_num].response, cursor)
+        result = dbtools.run_query(test, responses[problem_num].response, cursor)
 
         # Compare the student's code to the results.
         # TODO check sorting order, schema
         if str(sorted(expected)) != str(sorted(result)):
+          # TODO: print out the wrong query
           num_points -= test_points
 
       # If the code doesn't work, they don't get any points.
       except Exception:
+        # TODO print out the error
         # Print out the non-working code just in case it was a syntax error.
         print "Non-working code: \"" + responses[problem_num].response + "\""
         num_points -= test_points
@@ -84,55 +87,6 @@ def grade(specs, filename):
 
   print "Total points: " + str(total_points)
   cursor.close()
-
-
-def source_files(files, cursor):
-  """
-  source_files
-  ------------
-  Sources files into the database. Since the "source" command is for the
-  MySQL command-line interface, we have to parse the source file and run
-  each command one at a time.
-
-  files: The source files to source.
-  cursor: The database cursor.
-  """
-
-  if len(files) <= 0:
-    return
-
-  # Loop through each source file.
-  for source in files:
-    sql = open(source).read()
-    sql_list = sqlparse.split(sql)
-    for sql in sql_list:
-      if len(sql.strip()) == 0:
-        continue
-      cursor.execute(sql)
-
-
-def run_query(test, query, cursor):
-  """
-  run_query
-  ---------
-  test: The test to run. Contains information about the source files, the
-        setup, and the teardown necessary for each test.
-  query: The query to run.
-  cursor: The database cursor.
-  returns: The result of running the query.
-  """
-  result = []
-
-  # Source files if needed.
-  source_files(test["source"], cursor)
-
-  # Query setup.
-  cursor.execute(test["setup"])
-  cursor.execute(query)
-
-  result = [str(row) for row in cursor]
-  cursor.execute(test["teardown"])
-  return result
 
 
 def parse_file(f):
@@ -178,26 +132,16 @@ if __name__ == "__main__":
   # Parse command-line arguments.
   parser = argparse.ArgumentParser(description="Get the arguments.")
   parser.add_argument("--specs")
-  # The files to grade. Can be one or more files, or all files.
+  # The files to grade. Can be one or more files, or all files if * or nothing
+  # is specified.
   parser.add_argument("--files", nargs="+")
   args = parser.parse_args()
   (specs, files) = (args.specs, args.files)
 
   # If no arguments specified.
-  if specs is None or files is None:
-    print "Usage: main.py --specs [spec folder] --files [files to check]"
+  if specs is None:
+    print "Usage: main.py --specs <spec folder> [--files <files to check>]"
     sys.exit()
-
-  print "\n\n========================START GRADING========================\n\n"
-    
-  # TODO: Handle homeworks with multiple file submissions
-
-  # IF * is specified as files, then find all files of the same form.
-  if files[0] == "*":
-    # TODO: deal with directories
-    # TODO only find files corresponding to this spec
-    # TODO: need to untar stuff
-    files = [f for f in os.listdir(".")] # TODO f.startswith(... etc)
 
   # Open the spec folder to find the spec file. This file is a JSON string.
   # Convert it into a JSON object.
@@ -205,6 +149,21 @@ if __name__ == "__main__":
   specs = json.loads("".join(spec_file.readlines()))
   spec_file.close()
 
+  print "\n\n========================START GRADING========================\n\n"
+
+  # If * or nothing is specified for the files, grade all the files in the
+  # assignment. This is in the specs file.
+  if files is None or files[0] == "*":
+    files = specs["files"]
+    
+    # TODO go through each subdirectory and get all files
+    # TODO: deal with directories
+    # TODO only find files corresponding to this spec
+    # TODO: need to untar stuff
+    files = [f for f in os.listdir(".")] # TODO f.startswith(... etc)
+
+# TODO stopped here,
+# want to grade per student, then for each student, per file
   # Grade each file.
   for f in files:
     grade(specs, f)
