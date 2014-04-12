909c0a790e29e221953532b63c0eb8274f63fca2
Angela Gong <angela@caltech.edu>
2014-04-12 13:38:01 -0700

    Added DELETE and updated INSERT statements. - Wrote DELETE statement tests (but haven't tested). - INSERT tests now are done with transactions.

--- src/problemtype/create.py
+++ src/problemtype/create.py
@@ -4,14 +4,27 @@ class Create(ProblemType):
   """
   Class: Create
   -------------
-  Tests a create statement. TODO
+  Tests a create table statement. Checks for the following things:
+  - TODO
   """
 
   def grade_test(self, test, output):
+    # TODO parse the table name or specify in the specs?
+    
+    
+    
+    
+    
+    
+    
+    
+    
     output["success"] = "UNDETERMINED"
     # TODO create table statements are just printed.
+    
+    output["a"] = "asf"
     return 0
 
 
   def output_test(self, o, test, specs):
-    pass
+    o.write(str(test["a"]))
--- src/problemtype/insert.py
+++ src/problemtype/insert.py
@@ -5,36 +5,48 @@ class Insert(ProblemType):
   Class: Insert
   -------------
   Tests an insert statement and sees if the student's query produces the
-  same diff as the solution query.
+  same diff as the solution query. Does this using transactions to ensure the
+  database is not modified incorrectly.
   """
-
   def grade_test(self, test, output):
+    # Get the state of the table before the insert.
     table_sql = "SELECT * FROM " + test["table"]
+    before = self.db.run_query(table_sql)
 
-    # Compare the expected rows inserted versus the actual.
+    # Start a transaction and run the student's insert query. Assert that it
+    # IS an insert statement and is only a single statement (by checking that
+    # after removing the trailing semicolon, there are no more).
+    assert(self.response.sql.lower().find("insert") and \
+           self.response.sql.strip().rstrip(";").find(";") == 0)
+    self.db.start_transaction()
+    self.db.run_query(self.response.sql, setup=test.get("setup"), \
+                      teardown=test.get("teardown"))
+    actual = self.db.run_query(table_sql)
+    self.db.rollback()
+
+    # Make sure the rollback occurred properly.
+    assert(len(before.results) == len(self.db.run_query(table_sql).results))
+
+    # Start a transaction and run the solution insert statement.
+    self.db.start_transaction()
     self.cache.get(self.db.run_query, test["query"], \
                    setup=test.get("setup"), teardown=test.get("teardown"))
     expected = self.db.run_query(table_sql)
 
-    # Clean up the results of the test INSERT statement so the student's query
-    # can be run.
-    self.db.run_query("DELETE FROM " + test["table"])
-    self.db.run_query(self.response.sql, setup=test.get("setup"), \
-                      teardown=test.get("teardown"))
-    actual = self.db.run_query(table_sql)
-
-    # If the results are not equal in size, then they are wrong.
-    if len(expected.results) != len(actual.results):
+    # Compare the results of the test insert versus the actual. If the results
+    # are not equal in size, then it is automatically wrong. If the results are
+    # not the same, then they are also wrong.
+    if len(expected.results) != len(actual.results) or not \
+       self.equals(set(expected.results), set(actual.results)):
+      output["expected"] = list(set(before.results) - set(expected.results))
+      output["actual"] = list(set(before.results) - set(expected.results))
       return test["points"]
 
-    # If the results are equal, then then the test passed.
-    if self.equals(set(expected.results), set(actual.results)):
-      output["success"] = True
-      return 0
-
-    else:
-      return test["points"]
+    # Otherwise, their insert statement is correct.
+    output["success"] = True
+    return 0
 
 
   def output_test(self, o, test, specs):
+    # TODO
     pass
