db5f3416c54436af82926f022365291ea83209da
Angela Gong <angela@caltech.edu>
2014-06-02 17:58:53 -0700

    Added template secret file.

---
+++ src/secret.py
@@ -0,0 +1,11 @@
+# Database login information. Is a dictionary with the username as the key and
+# the password as the value.
+LOGIN = {
+  "user" : "pass"
+}
+
+# Database host.
+HOST = "host.com"
+
+# Database port.
+PORT = "5555"
